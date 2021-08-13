7712ed6bf4a0cc42a5a6a904d50d69b4ba60b875
Ivan Simeonov <nobu.siiw@akros.ch>
2021-08-13 12:57:11 +0300

    Initial commit

---
+++ src/main/java/com/tusofia/virtuallearningplatform/VirtualLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tusofia.virtuallearningplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualLearningPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tusofia/virtuallearningplatform/VirtuallearningplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tusofia.virtuallearningplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuallearningplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
